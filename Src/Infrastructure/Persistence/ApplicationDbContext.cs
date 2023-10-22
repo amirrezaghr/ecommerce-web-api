f8bbde524f1d94d2f8b177677e8465d97d4fb97d
ASUS <ASUS@LAPTOP-V1Q64D4R>
2023-10-22 13:03:56 +0330

    add dbContext and first Migration

---
+++ Src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+
+        }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
---
+++ Src/Web/ConfigureService.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web
+{
+    public static class ConfigureService
+    {
+        public static IServiceCollection AddWebServiceCollection(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
+            });
+            return builder.Services;
+        }
+    }
+}
